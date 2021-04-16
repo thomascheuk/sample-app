4de9d3d944aab86b8aa9d29cd821da194c339c87
thomascheuk <thomas.cheuk@gmail.com>
2021-04-16 15:39:56 +0800

    v1.0

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+./IBM_Cloud_CLI/ibmcloud plugin install -f code-engine
 ./IBM_Cloud_CLI/ibmcloud login -u apikey -p 5l3MxD2nKmP7WxGzu-Vn6D2fVHx2de9lV5fetJkqpzeh
-./IBM_Cloud_CLI/ibmcloud target -r au-syd -o owenchen@tw.ibm.com -s cheuk.sydney
+./IBM_Cloud_CLI/ibmcloud target -r jp-tok
+./IBM_Cloud_CLI/ibmcloud ce project select -n cheuk
 ./IBM_Cloud_CLI/ibmcloud ce application update -n sample-app
